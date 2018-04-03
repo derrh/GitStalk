99fa169e18ae1df5e7b44581de10d18b36d4bfd2
Derrick Hathaway <derr@me.com>
2018-04-02 21:40:32 -0600

    Initial Commit

---
+++ Repose/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Repose
+//
+//  Created by Derrick Hathaway on 4/2/18.
+//  Copyright © 2018 Derrick Hathaway. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = detailItem {
+        if let label = detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  var detailItem: NSDate? {
+    didSet {
+        // Update the view.
+        configureView()
+    }
+  }
+
+
+}
+
